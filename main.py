01d72e349bd87792d4668ead2582c842dc48349d
wChris7s <cw.aranibar.solaligue@gmail.com>
2024-11-05 13:04:56 -0500

    feat: dsa execution to main

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 from data_reader.dataset_loader import DatasetLoader
 from data_reader.dataset_types import DatasetType
+from distributed_algorithms.dsa_algorithm import \
+    DistributedSangerAlgorithm as dsa
+from distributed_algorithms.utility import initialize_parameters
 from network_config.graph_topology import GraphTopology
 from network_config.graph_types import GraphType
 
 
 def main():
-    graph = GraphTopology(GraphType.CYCLE_GRAPH, 20, 0.5)
+
+    k = 5
+    iterations = 10000
+    alpha = 0.1
+    number_of_nodes = 10
+    prob_connectivity = 0.5
+    step_strategy = 0
+
+    graph = GraphTopology(GraphType.CYCLE_GRAPH, number_of_nodes,
+                          prob_connectivity)
     graph.create_graph()
-    graph.show_graph()
 
     reader = DatasetLoader(DatasetType.MNIST)
     reader.load_data()
+
+    weight_matrix, initial_estimate, true_eigenspace = initialize_parameters(graph, reader, k)
+
+    dsa_algorithm = dsa(data=reader.get_data(),
+                        iterations=iterations,
+                        subspace_dimension=k,
+                        num_nodes=number_of_nodes,
+                        initial_estimate=initial_estimate,
+                        true_eigenspace=true_eigenspace)
+
+    angle_dsa = dsa_algorithm.execute(weight_matrix=weight_matrix,
+                                      step_size=alpha,
+                                      step_strategy=step_strategy)
+    print(angle_dsa)
 
 if __name__ == '__main__':
     main()
